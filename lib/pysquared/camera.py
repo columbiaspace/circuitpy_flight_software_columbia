f99e28b75ab868e37b5ab54e3f929a76c8652964
ineskhou <ik2512@columbia.edu>
2025-02-23 14:24:58 -0500

    added a delete function

--- lib/pysquared/camera.py
+++ lib/pysquared/camera.py
@@ -2,6 +2,7 @@ import board
 import sdioio
 import storage
 import camera
+import os
 
 
 class Sp_Camera():
@@ -29,4 +30,19 @@ class Sp_Camera():
             size = self.cam.take_picture(buffer, width=1920, height=1080, format=camera.ImageFormat.JPG)
             file.write(buffer, size)
             file.close()
+
+        #Deletes a picture and if it can't it returns -1
+        def delete_picture (self, file_name = "") -> int:
+               #File can't be deleted because it wasn't passed in
+            if file_name == "":
+                    return -1
+               
+            file_path = "/sd/" + file_name
+            if os.path.exists(file_path):
+                os.remove(file_path)   
+                return 1
+            else: 
+                  return -1
             
+
+        
