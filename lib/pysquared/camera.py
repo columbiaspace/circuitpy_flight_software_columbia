5732e744832e1d13949363edd4bde159bfc02fc1
ineskhou <ik2512@columbia.edu>
2025-02-23 14:04:44 -0500

    created with default code

---
+++ lib/pysquared/camera.py
@@ -0,0 +1,27 @@
+import board
+import sdioio
+import storage
+import camera
+
+# Initialize SD card storage
+sd = sdioio.SDCard(
+    clock=board.SDIO_CLOCK,
+    command=board.SDIO_COMMAND,
+    data=board.SDIO_DATA,
+    frequency=25000000)
+vfs = storage.VfsFat(sd)
+storage.mount(vfs, '/sd')
+
+# Set up camera, assign picture attributes, and take picture
+# Write picture data to file `buffer`.
+cam = camera.Camera()
+
+buffer = bytearray(512 * 1024)
+file = open("/sd/image.jpg","wb")
+size = cam.take_picture(buffer, width=1920, height=1080, format=camera.ImageFormat.JPG)
+file.write(buffer, size)
+file.close()
+
+def take_picture(self) -> None:
+        self.logger.info("Taking a Picture")
+
